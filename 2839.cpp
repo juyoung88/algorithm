ebe6760d367886f405f165360f5d41c3c3f3014d
unknown <thickishwharf9@gmail.com>
2021-12-22 20:47:34 +0900

    modified greedy alg

--- 2839.cpp
+++ 2839.cpp
@@ -6,14 +6,13 @@ int main()
 {
     int n,result=0;
     cin >> n;
-    result += n/5;
-    n%=5;
-    result += n/3;
-    n%=3;
-    if(n!=0)
-        cout << "-1";
-    else
-        cout << result;
+    while(n>=0) {
+        if(n%5==0){
+            result += (n/5);
+            cout << result;
+            return 0;
+        }
+    }
 
     return 0;
 }
