e4a6176b624831656a5720c7d247de47d02461a9
unknown <thickishwharf9@gmail.com>
2021-12-23 19:46:35 +0900

    Greedy alg

--- 2212.cpp
+++ 2212.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include <algorithm>
-
+#define MAX 10000
 using namespace std;
+
+ /*
+ input:
+ 6
+ 2
+ 1 6 9 3 6 7
+
+my thinking:
+sort() = 1 3 6 6 7 9
+at least on one point
+2 3 0 1 2
+sort() = 0 1 2 2 3
+6 - 2  = 4 connections is least required
+0 + 1 + 2 + 2 = 5 is least
+ */
+
 
 int main()
 {
-    
+    int n,k;
+    cin >> n >> k;
+    int sens[MAX];
+    int con[MAX];
+    for(int i=0;i<n;i++){
+        cin >> sens[i];
+    }
+    sort(sens,sens+n);
+    for(int i=0;i<n;i++)
+        cout << sens[i] << "\n";
+    for(int i=0;i<n-1;i++){
+        con[i] = sens[i+1] - sens[i];
+    }
+
 
 
 
