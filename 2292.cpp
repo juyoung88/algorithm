6ef330ad6d1182277330f312c12edc89336b0526
unknown <thickishwharf9@gmail.com>
2021-12-25 18:18:41 +0900

    basic math 1

---
+++ 2292.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i=0;
+    for(int sum=2;sum<=n;i++)
+        sum += 6*i;
+    if(n==1) i=1;
+    cout << i;
+    
+    return 0;
+}
