3fb68fb90bdd65754e19979f974cd4f6f4674088
unknown <thickishwharf9@gmail.com>
2021-12-29 14:23:42 +0700

    fixed bug

--- greedy/16953.cpp
+++ greedy/16953.cpp
@@ -4,26 +4,31 @@ using namespace std;
 
 int main()
 {
-    long long a,b;
+    int a,b;
     cin >> a >> b;
     int count=0;
     while(1){
-        if(b==a) break;
+        if(a>b) {
+            cout << "-1";
+            break;
+        }
+        if(b==a) {
+            cout << count+1;
+            break;
+        }
         if(b%10==1){
             b--;
             b/=10;
-            count++;
         }
         else if (b%2==0){
             b/=2;
-            count++;
         }
-        else break;
+        else {
+            cout << "-1";
+            break;
+        }
+        count++;
     }
-    if(a!=b)
-        cout << "-1";
-    else
-        cout << count+1;
 
     return 0;
 }
