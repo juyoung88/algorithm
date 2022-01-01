1b7bb1753d382ce49b144d5d2e5332bcae057d1b
unknown <thickishwharf9@gmail.com>
2022-01-01 21:04:19 +0700

    greedy alg w/ priority queue

---
+++ greedy/15903.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    priority_queue<long long,vector<long long>,greater<long long>> pq;
+    int answer=0;
+    cin >> n >> m;
+    // vector<int> nums;
+    int num;
+    for(int i=0;i<n;i++){
+        cin >> num;
+        pq.push(num);
+        // nums.push_back(num);
+    }
+    while(m>0){
+        long long a = pq.top();
+        pq.pop();
+        long long b=pq.top();
+        pq.pop();
+        long long sum = a+b;
+        pq.push(sum);
+        pq.push(sum);
+        m--;
+    }
+    while(!pq.empty()){
+        answer += pq.top();
+        pq.pop();
+    }
+    // sort(nums.begin(),nums.end());
+    
+    cout << answer;
+
+    return 0;
+}
