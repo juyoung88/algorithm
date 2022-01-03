dd41e6e6d6fcead2485ec9f28ae74c1e9910e1c0
unknown <thickishwharf9@gmail.com>
2022-01-03 14:06:02 +0700

    greedy alg

---
+++ greedy/9237.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> trees(n);
+    for(int i=0;i<n;i++)
+        cin >> trees[i];
+    sort(trees.begin(),trees.end());
+    for(int i=0;i<n;i++){
+        trees[i] -= i;
+    }
+    sort(trees.begin(),trees.end());
+    int answer = trees.back() + n + 1;
+    cout << answer;
+    
+
+    return 0;
+}
