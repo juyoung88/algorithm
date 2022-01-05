45c7c000e60406ee7cec3c707febeed12ecda180
unknown <thickishwharf9@gmail.com>
2022-01-05 19:48:16 +0700

    greedy alg

---
+++ greedy/1758.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> tips(n);
+
+    for(int i=0;i<n;i++)
+        cin >> tips[i];
+    sort(tips.begin(),tips.end(),greater<>());
+    int total=0;
+    for(int i=0;i<n;i++){
+        if((tips[i]-i)<0) continue;
+        total += tips[i] - (i);
+    }
+    cout << total;
+
+
+    return 0;
+}
