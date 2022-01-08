1f2ca97d96d5b175eea89050fc1cd66d1b43f774
unknown <thickishwharf9@gmail.com>
2022-01-08 21:43:04 +0700

    finalized prob

--- greedy/14241.cpp
+++ greedy/14241.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
+#include <queue>
 #include <algorithm>
 #include <vector>
 using namespace std;
-
 int main()
 {
     int n;
     cin >> n;
-    vector<int> score(n);
-    
+    vector<int> slimes(n);
     for(int i=0;i<n;i++){
-        cin >> score[i];
+        cin >> slimes[i];
     }
-    int total=score[0];
-    int add=score[0];
-    for(int i=1;i<n;i++){
-        total *=score[i];
-        add += total;
+    int sum=0;
+    while(n>1){
+        sort(slimes.begin(),slimes.end());
+
+        int back = slimes.back();
+        slimes.pop_back();
+
+        int back_2 = slimes.back();
+        slimes.pop_back();
+        slimes.push_back(back + back_2);
+        n--;
+        sum += back*back_2;
     }
-    cout << add;
+    cout << sum;
 
     return 0;
 }
