bcffb2c29f8f699072d769aa51cb8de1bc0cbd0b
Aidan <thickishwharf9@gmail.com>
2022-01-21 20:25:32 +0700

    graph traversal dfs bfs

---
+++ graph/1260.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <queue>
+using namespace std;
+    
+
+    
+bool visited[1000];
+vector<int> graph[10000];
+
+void dfs(int x) {
+        visited[x] = true;
+        cout << x << " ";
+        for(int i=0;i<graph[x].size();i++){
+            int y = graph[x][i];
+            if(!visited[y]) dfs(y);
+        }
+    }
+void bfs(int x) {
+    queue<int> q;
+    q.push(x);
+    visited[x]=true;
+    while(!q.empty()) {
+        int curr = q.front();
+        q.pop();
+        cout << curr << " ";
+        for(int i=0;i<graph[curr].size();i++){
+            int next = graph[curr][i];
+            if(!visited[next]){
+                visited[next]=true;
+                q.push(next);
+            }
+        }
+    }
+}
+int main()
+{   
+    int n,m,init,start,end;
+    cin >> n >> m >> init;
+    for(int i=0;i<m;i++){
+        cin >> start >> end;
+        graph[start].push_back(end);
+        graph[end].push_back(start);
+    }
+    for(int i=1;i<=n;i++)
+        sort(graph[i].begin(),graph[i].end());
+    dfs(init);
+    cout << "\n";
+    memset(visited,false,sizeof(visited));
+    bfs(init);
+    
+
+    return 0;
+}
