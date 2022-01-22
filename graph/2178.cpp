00594d5af0681b97401d132dd05b6f4f90559596
Aidan <thickishwharf9@gmail.com>
2022-01-22 19:37:23 +0700

    bfs maze

---
+++ graph/2178.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int graph[101][101];
+int n,m;
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,-1,1};
+
+int bfs(int x,int y) {
+    queue<pair<int,int>> q;
+    q.push({x,y});
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx < 0 || nx >=n || ny < 0 || ny >= m) continue;
+            if(graph[nx][ny]==0) continue;
+            if(graph[nx][ny]==1) {
+                graph[nx][ny]=graph[x][y]+1;
+                q.push({nx,ny});
+            }
+        }
+    }
+    return graph[n-1][m-1];
+}
+
+int main()
+{
+    cin >> n >> m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf("%1d",&graph[i][j]);
+        }
+    }
+    cout << bfs(0,0) << "\n";
+    return 0;
+}
